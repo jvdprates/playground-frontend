a394c79753faa70ac3e056750b0ffca8c408ed7e
João Prates <jvdprates@hotmail.com>
2020-10-11 11:07:42 -0300

    Adding visual transition

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 
+import { EmojiObjectsOutlined } from '@material-ui/icons';
+
 import "./styles.css";
 import { ThemeContext } from "../../contexts/ThemeContext";
-import ThemePicker, {ThemeType, defaultTheme} from "../../utils/themePicker";
+import ThemePicker, { ThemeType, defaultTheme } from "../../utils/themePicker";
 
 const MainPage: React.FC = (props) => {
-    const {selected, setTheme} = useContext(ThemeContext);
+    const { selected, setTheme } = useContext(ThemeContext);
     const [themeStyle, setThemeStyle] = useState<ThemeType>(defaultTheme);
 
     useEffect(() => {
@@ -14,19 +16,23 @@ const MainPage: React.FC = (props) => {
     }, [selected])
 
 
-    function handleChange(theme: string){
+    function handleChange(theme: string) {
         setTheme(theme);
     };
 
     return (
         <div className="mainBody" style={{ backgroundColor: themeStyle.lightColor }}>
             <div className="mainHeader" style={{ backgroundColor: themeStyle.mainColor }}>
-                <div className="mainIcon"></div>
-                <div className="mainTitle"></div>
+                <div className="mainIcon">
+                    <EmojiObjectsOutlined color="secondary"/>
+                </div>
+                <div className="mainTitle" style={{ color: themeStyle.secondaryColor }}>
+                    Programa de testes
+                </div>
                 <div className="mainButton">
                     {
-                        selected === "dark" ? <button onClick={(e) =>handleChange("light")}>Modo claro</button> 
-                        : <button onClick={(e) =>handleChange("dark")}>Modo escuro</button>
+                        selected === "dark" ? <button style={{ backgroundColor: themeStyle.lightColor, color: "black" }} onClick={(e) => handleChange("light")}>Modo claro</button>
+                            : <button style={{ backgroundColor: themeStyle.darkColor, color: "white" }} onClick={(e) => handleChange("dark")}>Modo escuro</button>
                     }
                 </div>
             </div>
